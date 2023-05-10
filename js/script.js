d34d30778812352d11aada5831f88fc021136ad0
Valentin <valentincordoba00@gmail.com>
2023-05-09 21:44:10 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document.getElementById("idheader").innerHTML = `
+<div class="header">
+<nav>
+  <label for="" >
+    <a href="index.html"> <img src="img/burger.png" alt="burgericon"></a>
+  </label>
+
+  <ul>
+  <a href="index.html"> INICIO</a>
+  <a href="menu.html"> MENU</a>
+  <a href="sucursales.html"> SUCURSALES</a>
+  <a href="delivery.html"> DELIVERY</a>
+  <a href="quienesSomos.html"> QUIENES SOMOS</a>
+  </ul>  
+</nav>
+</div>
+`
+document.getElementById("idfooter").innerHTML = `
+<div class="container">
+    <ul>
+      <li><a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+      <li> <a href="https://twitter.com/" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
+      <li><a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+      </ul>
+          
+      <p>&copy; 2023 BURGER</p>
+
+</div>
+`
